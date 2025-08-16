3c50e56a176ec986e25cf008b5d5721e91fa5564
PriyankaSharma1523 <sharmapriyanka8473@gmail.com>
2025-08-16 05:46:46 +0530

    Add files via upload
    Department of Accountancy's Digital Magazine

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function()
+ {
+    if (document.body.classList.contains('home-page'))
+ {
+        alert("Welcome to the Department of Accountancy's Digital Magazine!");
+    }
+};
+
